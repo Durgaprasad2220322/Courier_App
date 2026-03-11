f17a233add0d727ff3eedc904904f40d2209b319
Durgaprasad2220322 <kothurudurgaprasad7@gmail.com>
2026-03-11 20:51:16 +0530

    Create courierApppApplication.java

---
+++ src/main/java/com/courier/courierAppp/courierApppApplication.java
@@ -0,0 +1,13 @@
+package com.courier.courierAppp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierApppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierApppApplication.class, args);
+	}
+
+}
