1a3e3c3d4fdee03ff985c4a9f80d9015bceb7106
Durgaprasad2220322 <kothurudurgaprasad7@gmail.com>
2026-03-11 21:10:12 +0530

    Create UserRepository.java

---
+++ src/main/java/com/courier/courierAppp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.courier.courierAppp.repository;
+
+import java.util.Optional;
+import com.courier.courierAppp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
